4554937612e1cedc2789a35be8490063bc4957cd
Muhammad Siddique <siddique7777777@gmail.com>
2024-10-21 13:55:09 -0700

    Update card.tsx

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,9 +1,9 @@
-import image from '@/app/images.png'
+import Image from 'next/image'
 
 export default function Card() {
     return(
         <div className="card m-[45px] w-[200px] bg-slate-400 p-[10px] rounded-lg">
-            <img className="h-[100px] w-[100%] block rounded-md" src="https://static.feber.se/article_images/57/98/57/579857.jpeg" alt="" />
+            <Image className="h-[100px] w-[100%] block rounded-md" src="https://static.feber.se/article_images/57/98/57/579857.jpeg" alt="" />
             <div className="mt-[10px] font-bold text-lg">Title</div>
             <div className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores tempore ipsa nam qui repellendus.</div>
         </div>
